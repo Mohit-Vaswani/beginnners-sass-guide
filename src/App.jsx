f7f7efad7c732f1520a42feada3241ed7bf25bac
Mohit <mohitvaswani010@gmail.com>
2023-10-21 22:42:20 +0530

    added some basic concepts

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./styles/app.scss";
+
+const App = () => {
+  return (
+    <div id='app'>
+      <h1>Basics of SCSS / SASS</h1>
+      <ol>
+        <li>We can make variable in scss, so that we don't need to remember color code names and etc. <a target='_blank' href=''>click here to see code</a></li>
+        <li>We can also make an array of variable. <a target='_blank' href=''>click here to see code</a></li>
+      </ol>
+    </div>
+  )
+}
+
+export default App
